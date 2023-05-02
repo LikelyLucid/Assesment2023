b1577236b71f7fcadf186ba7156e86a88f491bd8
PurelyStupidity <micoolplays@gmail.com>
2023-05-02 21:45:47 +0000

    May 3, 2023, 9:45 AM

--- setup_1.py
+++ setup_1.py
@@ -1,8 +1,8 @@
 
 import pygame
-
+# initialize pygame
 pygame.init()
-
+# create the screen
 screen_width, screen_height = 600, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Highway Racer")
+pygame.display.set_caption("Highway Racer") # window title
