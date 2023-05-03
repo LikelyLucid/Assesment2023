be8c87581ee1ef560d89f7f2ad6da1d5c9a5977f
PurelyStupidity <micoolplays@gmail.com>
2023-05-03 21:10:33 +0000

    May 4, 2023, 9:10 AM

--- 2_Movement/Movement_1.py
+++ 2_Movement/Movement_1.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 # initialize pygame
 pygame.init()
 # create the screen
@@ -8,6 +9,17 @@ pygame.display.set_caption("Highway Racer") # window title
 
 # clock to control frame rate
 clock = pygame.time.Clock()
-
+black = (0,0,0)
 # import car
 car = pygame.image.load("../Assets/Player/Car.png")
+
+while True:
+    # run the game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    # update the screen
+    screen.fill(black)
+    # show car
+    screen.blit(car, (0,0))
+    clock.tick(60)
