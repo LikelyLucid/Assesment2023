aea6d4e4a0306a303edb75448b6f511a3aa7f85f
PurelyStupidity <micoolplays@gmail.com>
2023-05-03 21:19:41 +0000

    May 4, 2023, 9:19 AM

--- 2_Movement/Movement_1.py
+++ 2_Movement/Movement_1.py
@@ -7,13 +7,18 @@ pygame.init()
 # create the screen
 screen_width, screen_height = 600, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Highway Racer")  # window title
+pygame.display.set_caption("Highway Racer") # window title
 clock = pygame.time.Clock()
 # import car from ../Assets/Player/Car.png
 car = pygame.image.load(os.path.join("Assets", "Player", "Car.png"))
 
+car = pygame.transform.rotate(car, -90)
+
+car_size = 0.75
+resized_car = pygame.transform.scale(car, (100*car_size, 200*car_size))
+
 # get the car's dimensions
-car_width, car_height = car.get_rect().size
+car_width, car_height = resized_car.get_rect().size
 
 # set the car's initial position to the center of the screen
 car_x = (screen_width - car_width) / 2
@@ -31,7 +36,7 @@ while True:
     screen.fill((0, 0, 0))
 
     # blit the car onto the screen at its current position
-    screen.blit(car, (car_x, car_y))
+    screen.blit(resized_car, (car_x, car_y))
 
     # update the screen
     pygame.display.update()
