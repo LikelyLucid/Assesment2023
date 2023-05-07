09e00984b4b50eac2d12dbedda623516586a97fa
PurelyStupidity <micoolplays@gmail.com>
2023-05-07 23:53:11 +0000

    May 8, 2023, 11:53 AM

--- 3_Background/Background_1.py
+++ 3_Background/Background_1.py
@@ -1,35 +1,47 @@
 import pygame
 import os
+class Road:
+    def __init__(self, speed, screen_height):
+        self.speed = speed
+        self.screen_height = screen_height
+        self.image = pygame.image.load(os.path.join("Assets", "Roads", "Road1.jpg")) # replace "road.png" with your own image file
+        self.rect = self.image.get_rect()
+        self.rect.bottom = self.screen_height
+        self.y = self.rect.y
 
-ROAD_SPEED = 5
+    def update(self):
+        self.y += self.speed
+        if self.rect.top >= self.screen_height:
+            self.y = self.rect.y % self.screen_height
+            self.rect.bottom = self.screen_height + self.y
 
-# create the screen
-screen_width, screen_height = 600, 800
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Highway Racer")  # window title
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        if self.rect.top < 0:
+            rect = self.rect.copy()
+            rect.bottom = self.y
+            surface.blit(self.image, rect)
 
-# import Road from ../Assets/Roads/Road1.png
-background = pygame.image.load(os.path.join("Assets", "Roads", "Road1.jpg"))
-# rotate 90 degrees
-background = pygame.transform.rotate(background, -90)
-# scale to fill the entire screen
-background = pygame.transform.scale(background, (screen_width, screen_height))
-
-class Road():
-    def __init__(self, y, width, height):
-        self.y = y
-        self.width = width
-        self.height = height
-    def move(new_y)
-        
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-screen.blit(background, (0, 0))
-while True:
+all_sprites = pygame.sprite.Group()
+road = Road(5, screen.get_height())
+all_sprites.add(road)
+
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-    screen.blit(background, (0, 0))
-    pygame.display.update()
+            running = False
+
+    all_sprites.update()
+
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
     clock.tick(60)
+
+pygame.quit()
