97361fbf1e9e1c0a9f81d59efdaa36d230c99b51
PurelyStupidity <micoolplays@gmail.com>
2023-05-23 02:39:10 +0000

    May 23, 2023, 2:39 PM

--- 6_Menu/Menu_1.py
+++ 6_Menu/Menu_1.py
@@ -4,3 +4,9 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.font.init()
+font = pygame.font.SysFont("Arial", 40)
+score = 158
+def start_menu():
+
+def restart_menu():
