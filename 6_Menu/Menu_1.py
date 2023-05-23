389872dbc465211e4b41ffdb983d576a9226c911
PurelyStupidity <micoolplays@gmail.com>
2023-05-23 02:50:50 +0000

    May 23, 2023, 2:50 PM

--- 6_Menu/Menu_1.py
+++ 6_Menu/Menu_1.py
@@ -1,11 +1,14 @@
 import pygame
-
+import os
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.font.init()
 font = pygame.font.SysFont("Arial", 40)
+
+menu = pygame.image.load(os.path.join("Assets", "Menu", "Start_Menu.png"))
+
 score = 158
 def start_menu():
 
