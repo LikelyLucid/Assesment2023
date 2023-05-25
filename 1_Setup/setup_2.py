9f557e7e2a544a578297bf9b5a022c736eafe3db
PurelyStupidity <micoolplays@gmail.com>
2023-05-25 03:39:48 +0000

    May 25, 2023, 3:39 PM

---
+++ 1_Setup/setup_2.py
@@ -0,0 +1,13 @@
+
+import pygame
+# initialize pygame
+pygame.init()
+# create the screen
+screen_width, screen_height = 600, 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Highway Racer") # window title
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
